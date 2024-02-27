0738d963e520a0c759d107611b2402607e98142d
myanhluu1412 <161430433+myanhluu1412@users.noreply.github.com>
2024-02-27 14:49:11 +0700

    Add files via upload
    Create card

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'From HGL';
+const giftUrl = 'https://hgl.com.vn/';
+const eventName = 'Chúc mừng ngày 8/3';
+const titleCard = 'Cảm ơn một nửa thế giới vì đã luôn hoàn thành vai trò giữa gia đình và sự nghiệp.';
+const contentCard = 'Nhân ngày Quốc tế Phụ nữ 08/3, HGL xin gửi lời chúc đến những người bà, người mẹ, những chị em phụ nữ có một ngày thật vui vẻ, hạnh phúc bên những người yêu thương. Chúc cho “những bông hoa xinh đẹp” ấy có thật nhiều sức khỏe, luôn tươi tắn, rạng rỡ và tỏa sáng.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'women-day.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
+
